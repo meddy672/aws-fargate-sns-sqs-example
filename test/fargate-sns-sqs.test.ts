d816bf613645a92105666a9df947dff16c457adf
Matthew Eddy <meddy672@gmail.com>
2022-07-24 22:47:19 -0400

    Initial commit

---
+++ test/fargate-sns-sqs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FargateSnsSqs from '../lib/fargate-sns-sqs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/fargate-sns-sqs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FargateSnsSqs.FargateSnsSqsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
